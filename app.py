17653fdbc6bdcae95272a1bfc4a14ca5a6fe5787
Nathan Drake <nathan@mail-hub.co.uk>
2024-03-02 17:31:17 +0000

    Add Search functionality

--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def get_coffee():
     return render_template("coffee.html", coffee=coffee)
 
 
+@app.route("/search", methods=["GET", "POST"])
+def search():
+    query = request.form.get("query")
+    coffee = list(mongo.db.coffee.find({"$text": {"$search": query}}))
+    return render_template("coffee.html", coffee=coffee)
+
+
 @app.route("/register", methods=["GET", "POST"])
 def register():
     if request.method == "POST":
